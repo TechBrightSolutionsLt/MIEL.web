cdbff4f5ef1c9dbbec5cdfd719389deac32fc205
TechBrightSolutionsLt <techbrightfb@gmail.com>
2026-02-02 14:41:06 +0530

    Add files via upload

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MIEL.web.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        
+    }
+    
+}
