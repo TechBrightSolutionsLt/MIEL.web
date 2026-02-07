d94bb22a989cac5f42e20477a03cf072b7168874
sajeeshma <sajeeshmaxavier03@gmail.com>
2026-02-07 12:12:47 +0530

    add category load in dynamically

---
+++ Models/ViewModel/indexcategoryVM.cs
@@ -0,0 +1,9 @@
+﻿namespace MIEL.web.Models.ViewModel
+{
+    public class indexcategoryVM
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
