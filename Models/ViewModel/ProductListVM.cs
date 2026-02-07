05e68e8bd2fca1b1a151c6bf6b9056bdecc32e83
sajeeshma <sajeeshmaxavier03@gmail.com>
2026-02-07 17:09:36 +0530

    CHANGE FOR PRODUCT DETAILS

--- Models/ViewModel/ProductListVM.cs
+++ Models/ViewModel/ProductListVM.cs
@@ -1,5 +1,15 @@
 ﻿namespace MIEL.web.Models.ViewModel
 {
+    public class ColorSizeVM
+    {
+        public string Color { get; set; }
+        public string Size { get; set; }
+    }
+    public class SpecificationVM
+    {
+        public string SpecName { get; set; }
+        public string SpecValue { get; set; }
+    }
     public class ProductListVM
     {
         public int ProductId { get; set; }
@@ -10,7 +20,11 @@
 
         public string ProductDescription { get; set; }
         public List<string> Images { get; set; }
+        //public List<string> Specifications { get; set; }
 
+        public List<ColorSizeVM> Variants { get; set; }
+        public List<SpecificationVM> Specificationsnew{ get; set; }
 
     }
+
 }
