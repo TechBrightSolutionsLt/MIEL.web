de239f78b3fd031822ad0ae30923b4243258a503
TechBrightSolutionsLt <techbrightfb@gmail.com>
2026-02-10 17:17:23 +0530

    sales models

---
+++ Migrations/20260210111351_salestable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MIEL.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class salestable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BatchNo",
+                table: "InventoryBatch",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BatchNo",
+                table: "InventoryBatch",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
---
+++ Models/ViewModel/SalesItemVM.cs
@@ -0,0 +1,22 @@
+﻿namespace MIEL.web.Models.ViewModel
+{
+    public class SalesItemVM
+    {
+        public int varientid { get; set; }
+
+        public string ProductName { get; set; }
+        public string VariantCode { get; set; }
+
+        public string BatchNo { get; set; }
+
+        public int Quantity { get; set; }
+
+        // ===== PRICING =====
+        public decimal SellingPrice { get; set; } // GST INCLUDED
+        public decimal DiscPercent { get; set; }
+        public decimal DiscAmount { get; set; }
+
+        public decimal TaxAmount { get; set; }     // 10% INCLUDED GST
+        public decimal NetAmount { get; set; }
+    }
+}
